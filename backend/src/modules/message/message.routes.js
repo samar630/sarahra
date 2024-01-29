e1ee89638bbd6ef5cf091e918efb7856fe1fd3ba
breadan <breadan2020@example.com>
2024-01-29 20:39:37 +0200

    route2

--- backend/src/modules/message/message.routes.js
+++ backend/src/modules/message/message.routes.js
@@ -1,19 +1,19 @@
-import express from "express";
+import express from 'express';
 import {
   addMessage,
   deleteMessage,
   getUserMessages,
   updateMessage,
-} from "../../modules/message/messag.controller.js";
-import validate from "../../middleware/validate.message.js";
-import { auth } from "../../middleware/auth.js";
-import { verifyToken, verifyUserAccess } from "../../middleware/verifyToken.js";
+} from '../../modules/message/messag.controller.js';
+import validate from '../../middleware/validate.message.js';
+import { auth } from '../../middleware/auth.js';
+import { verifyToken, verifyUserAccess } from '../../middleware/verifyToken.js';
 
 const messageRouter = express.Router();
 
-messageRouter.post("/", [validate.message, auth, verifyUserAccess], addMessage);
-messageRouter.get("/", [auth], getUserMessages);
-messageRouter.patch("/:id", [auth, verifyToken], updateMessage);
-messageRouter.delete("/:id", [auth, verifyToken], deleteMessage);
+messageRouter.post('/', [validate.message, auth], addMessage);
+messageRouter.get('/', [auth], getUserMessages);
+messageRouter.patch('/:id', [auth, verifyToken], updateMessage);
+messageRouter.delete('/:id', [auth, verifyToken], deleteMessage);
 
 export default messageRouter;
